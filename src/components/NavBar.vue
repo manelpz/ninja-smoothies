9577a0eb217878c895ac1a871dc40187a112cdcc
Emmanuel López Guerrero <manelpz@icloud.com>
2020-11-11 23:24:58 -0600

    adding navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="navbar">
+        <nav class="nav-extended indigo darken-2">
+                <div class="nav-content">
+                    <router-link to="">
+                        <span class="nav-title">Ninja smoothies</span>
+                    </router-link>
+                </div>
+        </nav>  
+    </div>
+</template>
