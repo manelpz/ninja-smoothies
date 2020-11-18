28c236022896a570630ef2d27a4807b88362f2f0
Emmanuel López Guerrero <manelpz@icloud.com>
2020-11-17 23:53:51 -0600

    adding styles

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,7 +2,7 @@
   <div class="index container">
     <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
       <div class="card-content">
-        <i class="material-icons">delete</i>
+        <i class="material-icons delete">delete</i>
         <h2 class="indigo-text">{{ smoothie.title }}</h2>
         <ul class="ingredients">
           <li v-for="(ing,index) in smoothie.ingredients" :key="index">
@@ -41,5 +41,13 @@ export default {
 }
 .index .ingredients li{
   display: inline-block;  
+}
+.index .delete{
+  position: absolute;
+  top: 4px;
+  right:4px;
+  cursor:pointer;
+  color:#aaa;
+  font-size:1.4em;
 }
 </style>
