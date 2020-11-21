2151212da7c6b9be54527781c01cf27761d59f15
Emmanuel López Guerrero <manelpz@icloud.com>
2020-11-20 21:08:03 -0600

    adding firebase

---
+++ src/firebase/init.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBsjG8mEULDD26xyQaZx5kvf_NylgQALJg",
+    authDomain: "udemy-ninja-smoothies-4afe2.firebaseapp.com",
+    databaseURL: "https://udemy-ninja-smoothies-4afe2.firebaseio.com",
+    projectId: "udemy-ninja-smoothies-4afe2",
+    storageBucket: "udemy-ninja-smoothies-4afe2.appspot.com",
+    messagingSenderId: "261952979249",
+    appId: "1:261952979249:web:beacbf6026a9b2e7dc3010",
+    measurementId: "G-HW5JTT147H"
+  };
+  firebase.initializeApp(config);
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
-<!-- import firebase-->
+
 Vue.use(Router)
 
 export default new Router({
