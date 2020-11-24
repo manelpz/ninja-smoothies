cdce255d1bc4e929e85aa0844d4573a6a52f83f5
Emmanuel López Guerrero <manelpz@icloud.com>
2020-11-23 21:56:03 -0600

    adding add smoothie

---
+++ src/components/AddSmoothie.vue
@@ -0,0 +1,18 @@
+<template>
+    <h2>Add smoothie</h2>
+</template>
+
+<script>
+export default{
+    name:'AddSmoothie',
+    data () {
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
