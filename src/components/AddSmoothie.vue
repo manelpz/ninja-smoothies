9b846ca4a051120d17c448a5d05002598c4ffd70
Emmanuel López Guerrero <manelpz@icloud.com>
2020-11-23 22:28:26 -0600

    adding form

--- src/components/AddSmoothie.vue
+++ src/components/AddSmoothie.vue
@@ -1,5 +1,18 @@
 <template>
-    <h2>Add smoothie</h2>
+  <div class="add-smoothie container">
+        <h2 class="center-align indigo-text">Add new smoothie Recipe</h2>
+        <form>
+            <div class="field title">
+                <label for="title">Smoothie</label>
+                <input type="text" name="title" v-model="title">
+            </div>
+            <div class="field add-ingredient">
+                <label for="add-ingredient">Add an ingredient:</label>
+                <input type="text" name="add-ingredient">
+            </div>
+            <div class="field center-"></div>
+        </form>
+  </div>
 </template>
 
 <script>
