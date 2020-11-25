c642d4cea98c260db4af254b0c3112e32db9b4b0
Emmanuel López Guerrero <manelpz@icloud.com>
2020-11-24 23:56:01 -0600

    adding event

--- src/components/AddSmoothie.vue
+++ src/components/AddSmoothie.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="add-smoothie container">
         <h2 class="center-align indigo-text">Add new smoothie Recipe</h2>
-        <form>
+        <form @submit.prevent="AddSmoothie">
             <div class="field title">
                 <label for="title">Smoothie</label>
                 <input type="text" name="title" v-model="title">
             </div>
             <div class="field add-ingredient">
                 <label for="add-ingredient">Add an ingredient:</label>
-                <input type="text" name="add-ingredient">
+                <input type="text" name="add-ingredient" @keydown.tab="addIng" v-model="another">
             </div>
             <div class="field center-align">
                 <button class="btn pink">Add Smoothie</button>
@@ -22,7 +22,12 @@ export default{
     name:'AddSmoothie',
     data () {
         return{
-
+            title: null
+        }
+    },
+    methods:{
+        AddSmoothie(){
+            console.log(this.title)
         }
     }
 }
