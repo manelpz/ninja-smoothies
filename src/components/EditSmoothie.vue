c92da7bef18529e508cddf77d855e7879b2f98c8
Emmanuel López Guerrero <manelpz@icloud.com>
2020-12-02 23:20:06 -0600

    adding edit smoothie route

--- src/components/EditSmoothie.vue
+++ src/components/EditSmoothie.vue
@@ -3,3 +3,18 @@
         <h2>Edit a smoothie</h2>
     </div>
 </template>
+
+<script>
+    export default{
+    name:'EditSmoothie',
+    data () {
+        return {
+
+        }
+    }
+    }
+</script>
+
+<style>
+    
+</style>
